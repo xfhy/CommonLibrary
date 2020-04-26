12206fa6b7ec628b49127ee5ea8c0d3ae1bda88b
xfhy <xfhy666@qq.com>
2020-04-26 17:40:32 +0800

    精简一些东西,去掉Library中的version参数

--- Library/src/main/java/com/xfhy/library/service/InitIntentService.kt
+++ Library/src/main/java/com/xfhy/library/service/InitIntentService.kt
@@ -2,7 +2,6 @@ package com.xfhy.library.service
 
 import android.app.IntentService
 import android.content.Intent
-import com.alibaba.android.arouter.launcher.ARouter
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
 import com.xfhy.library.BuildConfig
@@ -28,10 +27,10 @@ class InitIntentService : IntentService("InitIntentService") {
         })
 
         //ARouter
-        if (BuildConfig.DEBUG) {  // 这两行必须写在init之前，否则这些配置在init过程中将无效
+        /*if (BuildConfig.DEBUG) {  // 这两行必须写在init之前，否则这些配置在init过程中将无效
             ARouter.openLog()     // 打印日志
             ARouter.openDebug()   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
         }
-        ARouter.init(application)   // 尽可能早，推荐在Application中初始化
+        ARouter.init(application)   // 尽可能早，推荐在Application中初始化*/
     }
 }
